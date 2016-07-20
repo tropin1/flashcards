3208eb7473a077347dd09791597f3dfef5c52061
tropin1 <kulin.valka@yandex.ru>
2016-07-20 19:29:43 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,8 @@
 require 'open-uri' 
 require 'nokogiri'
 page = Nokogiri::HTML(open('http://russian.languagedaily.com/wordsandphrases/russian-cognates'))
-	page.css('.jsn-article-content li').each do |links|
-	last_text = links.text.split('---').last
-	first_text = links.text.split('---').first
-	Card.create!(original_text: first_text, translated_text: last_text)
+page.css('.jsn-article-content li').each do |links|
+  last_text = links.text.split('---').last
+  first_text = links.text.split('---').first
+  Card.create!(original_text: first_text, translated_text: last_text)
 end
